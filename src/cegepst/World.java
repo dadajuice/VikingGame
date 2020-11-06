7af5d9d762fe7bfaf61cf7425eb937fd279e478f
dadajuice <davidt2003@msn.com>
2020-11-06 16:28:20 -0500

    Ajout de collisions pour le monde

--- src/cegepst/World.java
+++ src/cegepst/World.java
@@ -5,18 +5,32 @@ import cegepst.engine.Buffer;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class World {
 
     private static final String MAP_PATH = "images/demo.png";
     private Image background;
+    private ArrayList<Blockade> worldBorders;
 
     public World() {
         loadBackground();
+        worldBorders = new ArrayList<>();
+        Blockade topBorder = new Blockade();
+        topBorder.setDimension(400, 48);
+        topBorder.teleport(0, 0);
+        Blockade topBorder2 = new Blockade();
+        topBorder2.setDimension(400, 48);
+        topBorder2.teleport(464, 0);
+        worldBorders.add(topBorder);
+        worldBorders.add(topBorder2);
     }
 
     public void draw(Buffer buffer) {
         buffer.drawImage(background, 0, -64);
+        for (Blockade blockade : worldBorders) {
+            blockade.draw(buffer);
+        }
     }
 
     private void loadBackground() {
