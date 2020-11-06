8fa30937a844fc353ca2508eb7277168b55442a4
dadajuice <davidt2003@msn.com>
2020-11-06 16:16:57 -0500

    Ajout de collision avec l'arbre

--- src/cegepst/VikingGame.java
+++ src/cegepst/VikingGame.java
@@ -8,12 +8,14 @@ public class VikingGame extends Game {
     private GamePad gamePad;
     private Player player;
     private World world;
+    private Tree tree;
 
     public VikingGame() {
         gamePad = new GamePad();
         player = new Player(gamePad);
         player.teleport(200, 200);
         world = new World();
+        tree = new Tree(300, 350);
     }
 
     @Override
@@ -22,12 +24,23 @@ public class VikingGame extends Game {
         if (gamePad.isQuitPressed()) {
             super.stop();
         }
+        if (player.getY() < tree.getY() + 52) {
+            tree.blockadeFromTop();
+        } else {
+            tree.blockadeFromBottom();
+        }
     }
 
     @Override
     public void draw(Buffer buffer) {
         world.draw(buffer);
-        player.draw(buffer);
+        if (player.getY() < tree.getY() + 52) { // 80 - 28 (tolérance)
+            player.draw(buffer);
+            tree.draw(buffer);
+        } else {
+            tree.draw(buffer);
+            player.draw(buffer);
+        }
     }
 
     @Override
